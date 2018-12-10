cf9acad1d02c618b768e829152bc252bb2477f6b
Theonse, Gonzalo <gonzalo.theonse@capgemini.com>
2018-12-10 17:30:15 +0530

    main updated

--- main.go
+++ main.go
@@ -1,29 +1,33 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"path"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/gtheonse/golang-angular/handlers"
 )
 
 func main() {
-    r := gin.Default()
-    r.NoRoute(func(c *gin.Context) {
-        dir, file := path.Split(c.Request.RequestURI)
-        ext := filepath.Ext(file)
-        if file == "" || ext == "" {
-            c.File("./ui/dist/ui/index.html")
-        } else {
-            c.File("./ui/dist/ui/" + path.Join(dir, file))
-        }
-    })
+	r := gin.Default()
+	r.NoRoute(func(c *gin.Context) {
+		dir, file := path.Split(c.Request.RequestURI)
+		ext := filepath.Ext(file)
+		if file == "" || ext == "" {
+			c.File("./ui/dist/ui/index.html")
+		} else {
+			c.File("./ui/dist/ui/" + path.Join(dir, file))
+		}
+	})
 
-    r.GET("/todo", handlers.GetTodoListHandler)
-    r.POST("/todo", handlers.AddTodoHandler)
-    r.DELETE("/todo/:id", handlers.DeleteTodoHandler)
-    r.PUT("/todo", handlers.CompleteTodoHandler)
+	r.GET("/todo", handlers.GetTodoListHandler)
+	r.POST("/todo", handlers.AddTodoHandler)
+	r.DELETE("/todo/:id", handlers.DeleteTodoHandler)
+	r.PUT("/todo", handlers.CompleteTodoHandler)
 
-    err := r.Run(":3000")
-    if err != nil {
-        panic(err)
-    }
+	err := r.Run(":3000")
+	if err != nil {
+		panic(err)
+	}
 }
